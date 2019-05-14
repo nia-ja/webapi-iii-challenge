570837ef28d1641149ae5fa4685bca4587aa58b6
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-14 12:01:03 -0700

    added DELETE and PUT endpoints in userRouter

--- users/userRouter.js
+++ users/userRouter.js
@@ -63,13 +63,35 @@ router.get('/:id/posts', async (req, res) => {
       }
 });
 
-// router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+    try {
+        const count = await Users.remove(req.params.id);
+        if (count > 0) {
+          res.status(200).json({ message: 'The user has been deleted' });
+        } else {
+          res.status(404).json({ message: 'The user could not be found' });
+        }
+      } catch (error) {
+        res.status(500).json({
+          message: 'Error removing the user',
+        });
+      }
+});
 
-// });
-
-// router.put('/:id', (req, res) => {
-
-// });
+router.put('/:id', async (req, res) => {
+    try {
+        const user = await Users.update(req.params.id, req.body);
+        if (user) {
+          res.status(200).json({id: req.params.id, ...req.body});
+        } else {
+          res.status(404).json({ message: 'The user could not be found' });
+        }
+      } catch (error) {
+        res.status(500).json({
+          message: 'Error updating the user',
+        });
+      }
+});
 
 //custom middleware
 
