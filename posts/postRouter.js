8e5d9c69e9520e672b511617d4af1e74f8118a0d
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-14 13:09:50 -0700

    added all GET endpoints for postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,31 @@
-const express = 'express';
+const express = require('express');
+
+const Posts = require('./postDb.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get();
+        res.status(200).json(posts);
+      } catch (error) {
+        res.status(500).json({
+          message: 'Error retrieving the posts',
+        });
+      }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.getById(req.params.id);
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    } catch (error) {
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    }
 });
 
 router.delete('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const UserRouter = require('./users/userRouter.js');
+const PostsRouter = require('./posts/postRouter.js');
 
 const server = express();
 
@@ -8,6 +9,7 @@ server.use(express.json());
 server.use(myLogger); // should be on top of applying of router as well, otherwise is triggered by errors only
 
 server.use('/api/users', UserRouter);
+server.use('/api/posts', PostsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
