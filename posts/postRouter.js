69c3cb7af42802cc365c862ee6828e021fb8e6a0
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-14 16:22:57 -0700

    Finished CRUD operations on posts, added validatePostId custom middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -15,31 +15,54 @@ router.get('/', async (req, res) => {
       }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validatePostId, async (req, res) => {
+    res.status(200).json(req.post);
+});
+
+router.delete('/:id', validatePostId, async (req, res) => {
     try {
-        const post = await Posts.getById(req.params.id);
-        if (post) {
-            res.status(200).json(post);
+        const count = await Posts.remove(req.params.id);
+        if (count > 0) {
+          res.status(200).json({ message: 'The post has been deleted' });
         } else {
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
+          res.status(404).json({ message: 'The post could not be found' });
         }
-    } catch (error) {
-        res.status(500).json({ error: "The post information could not be retrieved." });
-    }
+      } catch (error) {
+        res.status(500).json({
+          message: 'Error removing the post',
+        });
+      }
 });
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, async (req, res) => {
+    try {
+        const post = await Posts.update(req.params.id, req.body);
+        if (post) {
+          res.status(200).json({id: req.params.id, ...req.body});
+        } else {
+          res.status(404).json({ message: 'The post could not be found' });
+        }
+      } catch (error) {
+        res.status(500).json({
+          message: 'Error updating the post',
+        });
+      }
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-
+async function validatePostId(req, res, next) {
+    try {
+        const post = await Posts.getById(req.params.id);
+        if (post) {
+            req.post = post;
+            next();
+        } else {
+            res.status(404).json({ message: "Invalid post id" })
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Failed to process request" });
+    }
 };
 
 module.exports = router;
